a3172ceb61ef99e9e547c3494e09bc40f1dd0f2e
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-09-24 19:06:06 +0200

    Dummysensor implemented

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using Avalonia.Controls.Documents;
+    using System;
+    using System.Linq;
+
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket DummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.DummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.DummyPacket;
+        }
+
+        public override void Process()
+        {
+            var carCoordinates = World.Instance.ControlledCar;
+            var target = World.Instance.WorldObjects.OfType<Circle>().FirstOrDefault();
+            this.DummyPacket.DistanceX=Math.Abs(carCoordinates.X-target.X);
+            this.DummyPacket.DistanceY=Math.Abs(carCoordinates.Y-target.Y);
+
+        }
+
+    }
+}
