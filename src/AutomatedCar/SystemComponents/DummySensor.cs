1ba88e20b8bf45af33da327c9ed8e0e5c7386bcc
Balázs Zick <balazs.zick2@gmail.com>
2025-09-24 16:49:19 +0200

    DummySensor implemented

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,26 @@
+﻿namespace AutomatedCar.SystemComponents;
+
+using Models;
+using Packets;
+using System;
+using System.Linq;
+
+public class DummySensor : SystemComponent
+{
+    private DummyPacket dummyPacket;
+
+    public DummySensor(VirtualFunctionBus virtualFunctionBus) 
+        : base(virtualFunctionBus)
+    {
+        this.dummyPacket = new DummyPacket();
+        virtualFunctionBus.DummyPacket = this.dummyPacket;
+    }
+    public override void Process()
+    {
+        var currentCar = World.Instance.ControlledCar;
+        var targetCircle = World.Instance.WorldObjects.First(x => x.GetType() == typeof(Circle));
+
+        this.dummyPacket.DistanceX = Math.Abs(currentCar.X - targetCircle.X);
+        this.dummyPacket.DistanceY = Math.Abs(currentCar.Y - targetCircle.Y);
+    }
+}
